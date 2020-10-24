61d75f9d2e6f8f6f1b3d14b4ec9a72b5253c58a8
Dariyus Hadizadeh <70909311+DariyusHadizadeh19@users.noreply.github.com>
2020-10-23 21:21:19 -0400

    Add files via upload

---
+++ end.js
@@ -0,0 +1,5 @@
+const username = document.getElementById('username');
+const saveScore = document.getElementById('saveScore');
+const finalScore = document.getElementById('finalScore');
+const mostRecentScore = localStorage.getItem('mostRecentScore');
+finalScore.innerText = mostRecentScore;
